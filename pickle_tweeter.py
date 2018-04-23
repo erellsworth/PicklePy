542d637e9e07b7f70764f56da973de7ff3912c14
Edward <edward.ellsworth@gmail.com>
2018-04-22 19:38:55 -0700

    inseart records into database

--- pickle_tweeter.py
+++ pickle_tweeter.py
@@ -1,18 +1,41 @@
 #Setup database
 import sqlite3
-#db = sqlite3.connect("picklefork.sqlite")
-#cursor = db.cursor()
-#cursor.execute("create table stories (url, glazed, cake, due)")
+db = sqlite3.connect("picklefork.sqlite")
+cursor = db.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS stories (medium_id, title, url)")
 
+cursor.execute("SELECT * FROM stories")
+for row in cursor:
+    print(row[2])
+    
 #gather posts from Medium
 import requests
 import json
-url = 'https://medium.com/pickle-fork/latest/?count=300'
+print('fetching data...')
+publication_url = 'https://medium.com/pickle-fork/'
+url = publication_url + 'latest/?count=3'
 headers = {"Accept" : "application/json", 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 result = requests.get(url, headers=headers)
 data = result.content.decode().replace('])}while(1);</x>', '')
 parsed = json.loads(data)
-print(parsed['success'])
+
+if parsed['success']:
+    print('it worked!')
+    post_count = len(parsed['payload']['posts'])
+    print(post_count, "posts found")
+
+    #loop through posts:
+    for post in parsed['payload']['posts']:
+        print(post['title'])
+        url = publication_url + post['uniqueSlug']
+        cursor.execute("INSERT INTO stories (medium_id, title, url) VALUES (?,?,?)" , (post['id'], post['title'], url))
+else:
+    print('nope')
+
+#db.commit()    
+
+
+
 
 
 
