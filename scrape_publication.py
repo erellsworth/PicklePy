c77120f4e70715194d7283849b68e68bea137ad4
Edward <edward.ellsworth@gmail.com>
2018-04-23 22:09:08 -0700

    Polish publication scraper

--- scrape_publication.py
+++ scrape_publication.py
@@ -2,6 +2,7 @@ from setup_database import *
 #gather posts from Medium
 import requests
 import json
+import re
 print('fetching data...')
 publication_url = 'https://medium.com/pickle-fork/'
 url = publication_url + 'latest/?count=200'
@@ -10,18 +11,28 @@ result = requests.get(url, headers=headers)
 data = result.content.decode().replace('])}while(1);</x>', '')
 parsed = json.loads(data)
 
+RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
+
+def strip_emoji(text):
+    return RE_EMOJI.sub(r'', text)
+
 if parsed['success']:
-    print('data found')
     post_count = len(parsed['payload']['posts'])
     print(post_count, "posts found")
 
     #loop through posts:
     for post in parsed['payload']['posts']:
-        print('adding ' + post['title'])
+        title = strip_emoji(post['title'])
+        post_url = publication_url + post['uniqueSlug']
         author_key = post['creatorId']
         author = parsed['payload']['references']['User'][author_key]
-        url = publication_url + post['uniqueSlug']
-        cursor.execute("INSERT OR IGNORE INTO stories (medium_id, title, url, author, author_twitter, pub_date) VALUES (?,?,?,?,?,?)" , (post['id'], post['title'], url, author['name'], author['twitterScreenName'], post['latestPublishedAt']))
+        author_twitter = '';
+        if 'twitterScreenName' in author:
+            author_twitter = author['twitterScreenName']
+            
+        print('adding', title)
+        #add to database
+        cursor.execute("INSERT OR IGNORE INTO stories (medium_id, title, url, author, author_twitter, pub_date) VALUES (?,?,?,?,?,?)" , (post['id'], title, post_url, author['name'], author_twitter, post['latestPublishedAt']))
 else:
     print('nope')
 
--- send_tweets.py
+++ send_tweets.py
@@ -4,6 +4,6 @@ from twitter_connect import api
 #status = api.PostUpdate("I'm working on a Python script to automatically tweet stories from the archive. This is a test Tweet.")
 #print(status.text)
 
-cursor.execute("SELECT * FROM stories")
+cursor.execute("SELECT * FROM stories ORDER BY pub_date")
 for row in cursor:
-    print(row[3])
+    print(row[1])
