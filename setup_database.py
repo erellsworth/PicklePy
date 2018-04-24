cbc5edac033cc20a5f0c98a870bcbcd40e8c9bc7
Edward <edward.ellsworth@gmail.com>
2018-04-23 20:08:39 -0700

    add columns to stories table

--- setup_database.py
+++ setup_database.py
@@ -1,5 +1,5 @@
 #Setup database
 import sqlite3
-db = sqlite3.connect("picklefork.sqlite")
+db = sqlite3.connect("picklepy.sqlite")
 cursor = db.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS stories (medium_id, title, url)")
+cursor.execute("CREATE TABLE IF NOT EXISTS stories (medium_id, title, url, author, author_twitter)")
