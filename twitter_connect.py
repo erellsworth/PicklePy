44d1b173a021112508f2e9140e6ec8f931a62fe4
Edward <edward.ellsworth@gmail.com>
2018-04-23 22:55:48 -0700

    send tweet

--- twitter_connect.py
+++ twitter_connect.py
@@ -1,6 +1,7 @@
 #connect to Twitter API
 import twitter
 from twitter_vars import *
+#you will need to create your own twitter_vars.py file to store your API keys
 
 api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
