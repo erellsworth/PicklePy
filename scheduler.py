a8b85a0893059855083bd0c36495e869f08580ff
Edward <edward.ellsworth@gmail.com>
2018-04-25 23:37:26 -0700

    add another time

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ from send_tweet import send_tweet
 import schedule
 import time
 
-tweet_times = ["08:30", "14:30", "18:30"]
+tweet_times = ["06:30", "09:30", "14:30", "18:30"]
 
 for tweet_time in tweet_times:
     schedule.every().day.at(tweet_time).do(send_tweet)
